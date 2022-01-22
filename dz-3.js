5415857672217019a341a74982346e3417d062d7
nastiaSherr <91912351+nastiaSherr@users.noreply.github.com>
2022-01-22 15:26:33 +0200

    Add files via upload

---
+++ dz-3.js
@@ -0,0 +1,19 @@
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+
+const result = 5 + 5 + '5';
+console.log(typeof result);
+
+const my = "My";
+const name = "name";
+const is = "is";
+const fullname = (`${my} ${name} ${is} Vicktor`)
+console.log(fullname);
+
+const userName = "Олександро";
+const payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
+
+const email = "qwertyuiop";
+console.log(email.length);
+console.log(email.includes('@'));
